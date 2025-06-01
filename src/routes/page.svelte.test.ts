b072249fb6f45fcef96a123ebf8eeb455b25a31f
Ondra Hlaváč <ondra@hlavac.cz>
2025-06-01 14:19:13 +0200

    chore: delete all previous work and restart the whole project

--- src/routes/page.svelte.test.ts
+++
@@ -1,5 +0,0 @@
-import { describe, test, expect } from 'vitest';
-import '@testing-library/jest-dom/vitest';
-import { render, screen } from '@testing-library/svelte';
-import Page from './+page.svelte';
-
